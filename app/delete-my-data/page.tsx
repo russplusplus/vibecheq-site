eaea7aa82af5e4b04df8a46c561fec63ae687038
Russell Kerber <russell.kerber@gmail.com>
2025-01-09 09:57:15 -0700

    delete-my-data working

--- app/delete-my-data/page.tsx
+++ app/delete-my-data/page.tsx
@@ -1,92 +1,207 @@
-// import "../../app/globals.css";
-// import 'react-phone-number-input/style.css'
-// import PhoneInput, { type Value } from 'react-phone-number-input'
-// import { useState, useEffect } from 'react'
-// import { Spinner } from "@nextui-org/spinner";
-// import ClipLoader from "react-spinners/ClipLoader"
+"use client"
 
-// import { auth } from "../../firebase"
-// import firebase from 'firebase/app';
-// import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-// import { getDatabase } from "firebase/database"
-// import AuthContextProvider from "./AuthContext"
-import DeleteMyData from "./DeleteMyData"
+import "../../app/globals.css";
+import 'react-phone-number-input/style.css'
+import PhoneInput, { type Value } from 'react-phone-number-input'
+import { useState, useEffect } from 'react'
+import { Spinner } from "@nextui-org/spinner";
+import ClipLoader from "react-spinners/ClipLoader"
+
+import { getDatabase } from "firebase/database"
 
 import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged, User, Auth } from 'firebase/auth'
+// import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check"
+import { 
+    getAuth, 
+    onAuthStateChanged, 
+    RecaptchaVerifier, 
+    signInWithPhoneNumber, 
+    ConfirmationResult, 
+    User, 
+    deleteUser 
+} from 'firebase/auth'
+
+import dynamic from 'next/dynamic'
 
 const log = console.log.bind(console)
 
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-};
+const firebaseConfig = window.location.origin.includes("localhost") 
+? {
+    apiKey: "AIzaSyBS_IdUAjkyZaiiGosPPnr23mCn2MiItVk",
+    authDomain: "vibecheq-dev-d930b.firebaseapp.com",
+    databaseURL: "https://vibecheq-dev-d930b-default-rtdb.firebaseio.com",
+    projectId: "vibecheq-dev-d930b",
+    storageBucket: "vibecheq-dev-d930b.appspot.com",
+    messagingSenderId: "958818368953",
+    appId: "1:958818368953:web:a42c67cd34e1a81c268a68",
+    measurementId: "G-QM9V6YHLE2"
+}
+: {
+    apiKey: "AIzaSyDMDfZyu9ysYxKCfYOaDeBW75-ZkLrnQpI",
+    authDomain: "vibecheq-prod.firebaseapp.com",
+    databaseURL: "https://vibecheq-prod-default-rtdb.firebaseio.com",
+    projectId: "vibecheq-prod",
+    storageBucket: "vibecheq-prod.appspot.com",
+    messagingSenderId: "670048616046",
+    appId: "1:670048616046:web:473b754e7177448ce8707f",
+    measurementId: "G-9H7DQB1GY8"
+}
+  
 log('firebaseConfig:', firebaseConfig)
 
 const app = initializeApp(firebaseConfig);
 log('app initialized. app:', app)
 
 const auth = getAuth()
-// log('page auth:', auth)
 
-export default function DeleteMyDataContainer(props: any) {
+onAuthStateChanged(auth, user => {
+    if (user != null) {
+        log('logged in!')
+    } else {
+        log('no user')
+    }
+})
 
-    // const [phoneNumber, setPhoneNumber] = useState<Value>()
-    // const [isLoading, setLoading] = useState(false)
+export default function DeleteMyData(props: any) {
 
-    // const [appVerifier, setAppVerifier] = useState<RecaptchaVerifier | null>(null)
+    const [phoneNumber, setPhoneNumber] = useState<Value>()
+    const [otp, setOtp] = useState('')
+    const [isLoading, setLoading] = useState(false)
+    const [confirmationResult, setConfirmationResult] = useState<null | ConfirmationResult>(null)
+    const [user, setUser] = useState<null | User>(null)
+    const [userDeleted, setUserDeleted] = useState(false)
 
+    async function submit() {
+        log('in submit. phone number:', phoneNumber)
+        if (isLoading) {
+            log('already loading')
+            return
+        }    
+        setLoading(true)
 
-    
+        let verifier = new RecaptchaVerifier(auth, 'recaptcha-container', {
+            'size': 'invisible',
+            'callback': (response: any) => {
+              // reCAPTCHA solved, allow signInWithPhoneNumber.
+              log('reCAPTCHA solved \m| response:', response)
+            }
+        });
 
-    // async function onSignInSubmit(phoneNumber: string) {
-    //     const confirmationResult = await signInWithPhoneNumber(auth, phoneNumber, appVerifier).catch((err) => {
-    //         log('err:', err)
-    //         return
-    //     })
+        const result = await signInWithPhoneNumber(auth, String(phoneNumber), verifier)
+            .catch((err) => {
+                log('signInWithPhoneNumber error:', err)
+                window.location.reload()
+            })
 
-    //     log('confirmationResult:', confirmationResult)
+        log('result:', result)
+        if (result) setConfirmationResult(result)
+        setLoading(false)
+    }
+
+    async function confirmCode() {
+        if (isLoading) return
+
+        setLoading(true)
+        const result  = await confirmationResult?.confirm(otp)
+            .catch((err) => {
+                log('confirmCode error:', err)
+            })
         
-    //     // SMS sent. Prompt user to type the code from the message, then sign the
-    //     // user in with confirmationResult.confirm(code).
-    // }
+        if (result?.user) {
+            setUser(result.user)
+        } else {
+            log('error. no user.')
+        }
+        setLoading(false)
+    }
 
-    // async function submit() {
-    //     log('in submit')
-    //     setLoading(true)
-    //     // onSignInSubmit(String(phoneNumber))
-    // }
+    async function deleteData() {
+        if (isLoading) return
+        if (!user) return
+        setLoading(true)
 
-    // function setUpRecaptcha() {
-    //     log('auth:', auth)
-    //     window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
-    //         'size': 'invisible',
-    //         'callback': (response: any) => {
-    //           // reCAPTCHA solved, allow signInWithPhoneNumber.
-    //           log('reCAPTCHA solved \m| response:', response)
-    //         //   onSignInSubmit(String(phoneNumber));
-    //         }
-    //     });
-    //     // setAppVerifier(verifier)
-    // }
+        console.log('in deleteData. user:', user)
 
-    // useEffect(() => {
-    //     log('phoneNumber:', phoneNumber)
-    // }, [phoneNumber])
+        await deleteUser(user)
+            .catch((err) => {
+                log('deleteUser error:', err)
+            })
 
-    // useEffect(() => {
-    //     setUpRecaptcha()
-    // }, [])
+        setLoading(false)
+        setUserDeleted(true)
+    }
+
+    useEffect(() => {
+        log('phoneNumber:', phoneNumber)
+    }, [phoneNumber])
+
+    useEffect(() => {
+        log('otp:', otp)
+    }, [otp])
 
     return (
-        // <AuthContextProvider auth={JSON.stringify(auth)}>
-            <DeleteMyData authStr={JSON.stringify(auth)}/>
-        // </AuthContextProvider>
+        <div className="flex flex-col justify-center items-center h-screen w-screen" >
+            {userDeleted ?
+            <>
+                <div className="text-vwhite self-center">User data successfully deleted.</div>
+            </>
+            :
+            <>
+                {user ?
+                <>
+                    <div className="text-vwhite self-center">Delete data for user {user.phoneNumber}?</div>
+                    <div className="bg-vwhite text-center mt-3 w-40 items-self-center hover:cursor-pointer select-none" onClick={deleteData}>
+                        {isLoading 
+                            ? <ClipLoader
+                                size={14}
+                                color={"#303841"}
+                            />
+                            : <>Delete</>
+                        }
+                    </div>
+                </>
+                :
+                <>
+                    {confirmationResult ?
+                    <div className="flex flex-col items-center">
+                        <label className="text-vwhite self-start">Passcode:</label>
+                        <input onChange={(e) => setOtp(e.target.value)}></input>
+                        <div onClick={confirmCode} className="bg-vwhite text-center mt-3 w-40 items-self-center hover:cursor-pointer select-none">
+                            {isLoading 
+                            ? <ClipLoader
+                                size={14}
+                                color={"#303841"}
+                            />
+                            : <>Submit</>
+                            }
+                        </div>
+                    </div>
+                    :
+                    <div className="flex flex-col items-center">
+                        <label className="text-vwhite self-start" htmlFor="phoneInput">Phone number:</label>
+                        <PhoneInput
+                            className="bg-white"
+                            id="phoneInput"
+                            defaultCountry="US"
+                            value={phoneNumber}
+                            onChange={setPhoneNumber}
+                        />
+                        <div id="recaptcha-container"></div>
+                        <div id="sign-in-button" className="bg-vwhite text-center mt-3 w-40 items-self-center hover:cursor-pointer select-none" onClick={submit}>
+                            {isLoading 
+                            ? <ClipLoader
+                                size={14}
+                                color={"#303841"}
+                            />
+                            : <>Send passcode</>
+                            }
+                        </div>
+                    </div>
+                    }
+                </>
+                }
+            </>
+            }
+        </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// import { AuthContextProvider } from "../pages/delete-my-data/AuthContext"
-// import { initializeApp } from "firebase/app";
-// import { getAuth, onAuthStateChanged, User, Auth } from 'firebase/auth'
 
 const log = console.log.bind(console)
+
+
 
 export const metadata: Metadata = {
   title: "Vibecheq",
   description: "Vibecheq",
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     // <AuthContextProvider app={app} auth={auth}>
+    // <RootLayoutWrapper>
       <html lang="en">
         <body>
           {/* <AuthProvider> */}
@@ -25,6 +27,7 @@ export default function RootLayout({
           {/* </AuthProvider> */}
         </body>
       </html>
+    // </RootLayoutWrapper>
     // </AuthContextProvider>
   );
 }
