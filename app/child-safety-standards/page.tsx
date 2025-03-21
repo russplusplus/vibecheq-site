4aa1cf62e8be098458a48be1e5c4b6771f926e2c
Russell Kerber <russell.kerber@gmail.com>
2025-03-20 19:10:35 -0600

    child-safety-standards page created

---
+++ app/child-safety-standards/page.tsx
@@ -0,0 +1,163 @@
+export default function Page() {
+    return (
+        <div className="m-20 text-vwhite">
+            <h1 className="text-3xl mb-4">Child Safety Standards</h1>
+            <p>Effective Date: Thursday, March 20, 2025</p>
+            <p>Version: 1.0</p>
+            <p>
+                Vibecheq is committed to ensuring a safe and secure environment for all users of our platform, 
+                especially children. We take our responsibility seriously to prevent and address any harmful behavior, 
+                including Child Sexual Abuse and Exploitation (CSAE). This Child Safety Standards document outlines 
+                the specific measures we have put in place to protect children and ensure compliance with relevant 
+                laws and regulations.
+            </p>
+            <h1 className="text-3xl mb-4">
+                1. Zero Tolerance for Child Sexual Abuse and Exploitation (CSAE)
+            </h1>
+            <p>
+                Vibecheq strictly prohibits any form of Child Sexual Abuse and Exploitation (CSAE) on our platform. 
+                CSAE is a serious violation of human rights and is illegal under international law. We are committed 
+                to preventing, detecting, and responding to any CSAE-related activities.
+            </p>
+            <p>Key Prohibitions:</p>
+            <ul>
+                <li>
+                    <p>
+                        Sexual Exploitation: Any form of sexual exploitation, including but not limited to grooming, 
+                        solicitation, or trading in child sexual content, is strictly prohibited.
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Abuse Content: Content that involves child sexual abuse material (CSAM) or any sexually explicit material involving minors is strictly banned.
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Sexual Harassment: Any interaction that sexually exploits or harasses minors, whether in a direct or indirect manner, is prohibited.
+                    </p>
+                </li>
+            </ul>
+
+
+
+
+
+
+
+
+            <p>
+                This Privacy Policy (&quot;Policy&quot;) applies to Vibecheq, and Vibecheq, LLC (&quot;Company&quot;) and governs data collection and usage. 
+                For the purposes of this Privacy Policy, unless otherwise noted, all references to the Company include vibecheq.org. 
+                The Company&apos;s application is a social media application. By using the Company application, you consent to the data practices 
+                described in this statement. 
+            </p>
+            
+            <h2 className="text-xl mt-6">Collection of your Personal Information</h2>
+            <p>
+                In order to better provide you with products and services offered, the Company may collect personally identifiable information, 
+                such as your: 
+            </p>
+            <ul>
+                <li className="ml-2">- Phone number</li>
+            </ul>
+            <p>
+                We do not collect any personal information about you unless you voluntarily provide it to us. However, you may be required to 
+                provide certain personal information to us when you elect to use certain products or services. These may include: (a) registering 
+                for an account; (b) entering a sweepstakes or contest sponsored by us or one of our partners; (c) signing up for special offers 
+                from selected third parties; (d) sending us an email message; (e) submitting your credit card or other payment information when 
+                ordering and purchasing products and services. To wit, we will use your information for, but not limited to, communicating with 
+                you in relation to services and/or products you have requested from us. We also may gather additional personal or non-personal 
+                information in the future. 
+            </p>
+            
+            <h2 className="text-xl mt-6">Use of your Personal Information</h2>
+            <p>
+                The Company collects and uses your personal information in the following ways:  
+            </p>
+            <ul>
+                <li className="ml-2">- to operate and deliver the services you have requested </li>
+                <li className="ml-2">- to provide you with information, products, or services that you request from us </li>
+                <li className="ml-2">- to provide you with notices about your account </li>
+                <li className="ml-2">- to carry out the Company&apos;s obligations and enforce our rights arising from any contracts entered between you and us, including for billing and collection </li>
+                <li className="ml-2">- to notify you about changes to our Vibecheq or any products or services we offer or provide through it </li>
+                <li className="ml-2">- in any other way we may describe when you provide the information </li>
+                <li className="ml-2">- for any other purpose with your consent. </li>
+            </ul>
+            <p>
+                The Company may also use your personally identifiable information to inform you of other products or services available from the Company and its affiliates. 
+            </p>
+            
+            <h2 className="text-xl mt-6">Sharing Information with Third Parties </h2>
+            <p>
+                The Company does not sell, rent, or lease its customer lists to third parties. 
+            </p>
+            <p>
+                The Company may share data with trusted partners to help perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to the Company, and they are required to maintain the confidentiality of your information. 
+            </p>
+            <p>
+                The Company may disclose your personal information, without notice, if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on the Company or the site; (b) protect and defend the rights or property of the Company; and/or (c) act under exigent circumstances to protect the personal safety of users of the Company, or the public. 
+            </p>
+            
+            <h2 className="text-xl mt-6">Right to Deletion</h2>
+            <p>
+                Subject to certain exceptions set out below, on receipt of a verifiable request from you, we will: 
+            </p>
+            <ul>
+                <li className="ml-2">- Delete your personal information from our records; and </li>
+                <li className="ml-2">- Direct any service providers to delete your personal information from their records.</li>
+            </ul>
+            <p>
+                Please note that we may not be able to comply with requests to delete your personal information if it is necessary to: 
+            </p>
+            <ul>
+                <li className="ml-2">- Complete the transaction for which the personal information was collected, fulfill the terms of a written warranty or product recall conducted in accordance with federal law, and provide a good or service requested by you, or reasonably anticipated within the context of our ongoing business relationship with you, or otherwise perform a contract between you and us; </li>
+                <li className="ml-2">- Detect security incidents, protect against malicious, deceptive, fraudulent, or illegal activity; or prosecute those responsible for that activity; </li>
+                <li className="ml-2">- Debug to identify and repair errors that impair existing intended functionality; </li>
+                <li className="ml-2">- Exercise free speech, ensure the right of another consumer to exercise his or her right of free speech, or exercise another right provided for by law; </li>
+                <li className="ml-2">- Comply with the California Electronic Communications Privacy Act; </li>
+                <li className="ml-2">- Engage in public or peer-reviewed scientific, historical, or statistical research in the public interest that adheres to all other applicable ethics and privacy laws, when our deletion of the information is likely to render impossible or seriously impair the achievement of such research, provided we have obtained your informed consent; </li>
+                <li className="ml-2">- Enable solely internal uses that are reasonably aligned with your expectations based on your relationship with us; </li>
+                <li className="ml-2">- Comply with an existing legal obligation; or </li>
+                <li className="ml-2">- Otherwise use your personal information, internally, in a lawful manner that is compatible with the context in which you provided the information. </li>
+            </ul>
+            
+            <h2 className="text-xl mt-6">Children Under Thirteen</h2>
+            <p>
+                The Company does not knowingly collect personally identifiable information from children under the age of 13. If you are under the age of 13, you must ask your parent or guardian for permission to use this application. 
+            </p>
+
+            <h2 className="text-xl mt-6">External Data Storage Sites</h2>
+            <p>
+                We may store your data on servers provided by third-party hosting vendors with whom we have contracted. 
+            </p>
+
+            <h2 className="text-xl mt-6">Changes to This Statement</h2>
+            <p>
+                The Company reserves the right to change this Policy from time to time. For example, when there are changes in our services, changes in our data protection practices, or changes in the law. When changes to this Policy are significant, we will inform you. You may receive a notice by sending an email to the primary email address specified in your account, by placing a prominent notice on our Vibecheq, LLC, and/or by updating any privacy information. Your continued use of the application and/or services available after such modifications will constitute your: (a) acknowledgment of the modified Policy; and (b) agreement to abide and be bound by that Policy. 
+            </p>
+
+            <h2 className="text-xl mt-6">Copntact Information</h2>
+            <p>
+                The Company welcomes your questions or comments regarding this Policy. If you believe that the Company has not adhered to this Policy, please contact the Company at: 
+            </p>
+            <p>
+                Vibecheq, LLC 
+                2525 Arapahoe Ave Ste E4 #1002 
+                Boulder, Colorado 80302-6746 
+            </p>
+            <p>
+                Email Address: 
+                russell.kerber@gmail.com 
+            </p>
+            <p>
+                Phone Number: 
+                7194013520 
+            </p>
+            <p>
+                Effective as of December 29, 2024 
+            </p>
+        
+        </div>
+    )
+}
